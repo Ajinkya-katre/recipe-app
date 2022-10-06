7099f1d307a97edc77e6aa1ad7a369d235baea25
ajinkya katre <katreajinkya@gmail.com>
2022-10-06 12:10:15 +0530

    Recipe Data Added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is recipe demo','https://www.pngitem.com/pimgs/m/235-2354731_recipes-png-transparent-png.png'),
+    
+    new Recipe('A Test Recipe','This is recipe demo','https://www.pngitem.com/pimgs/m/235-2354731_recipes-png-transparent-png.png')
+
+  ];
 
   constructor() { }
 
